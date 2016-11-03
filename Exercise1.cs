f590ee1f3e22798e95449e3fe0a14ab141422b19
Razvan Sdira <sdira_razvan@yahoo.com>
2016-11-03 23:27:54 +0200

    Add files via upload

---
+++ Exercise1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_Data_Types
+{
+    class Exercise1
+    {
+        static void Main (String[] args)
+        {
+            char a, b, c;
+
+            Console.WriteLine("Enter letter: ");
+            a = Convert.ToChar(Console.ReadLine());
+
+            Console.WriteLine("Enter letter: ");
+            b = Convert.ToChar(Console.ReadLine());
+
+            Console.WriteLine("Enter letter: ");
+            c = Convert.ToChar(Console.ReadLine());
+
+            Console.WriteLine("Reverse order: ");
+            Console.WriteLine("{0} {1} {2}", c, b, a);
+
+            Console.ReadKey();
+        }
+    }
+}
