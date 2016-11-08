9c5c7c57f0e5f2a5153528f561253f8a8027fe0f
Razvan Sdira <sdira_razvan@yahoo.com>
2016-11-08 20:27:13 +0200

    Add files via upload

---
+++ Exercise9.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_Data_Types
+{
+    class Exercise9
+    {
+        static void Main(string[] args)
+        {
+            char x;
+            Console.Write("Input a symbol: ");
+            x = Convert.ToChar(Console.ReadLine());
+            if ((x == 'a') || (x == 'e') || (x == 'i') || (x == 'o') || (x == 'u'))
+                Console.WriteLine("It's a lowercase vowel.");
+            else if ((x >= '0') &&  (x <= '9'))
+                Console.WriteLine("It's a digit.");
+            else Console.WriteLine("Other symbol");
+            Console.ReadLine();
+        }
+    }
+}
