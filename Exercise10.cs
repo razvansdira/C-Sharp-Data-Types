88fcdab757d4210c2237e3ff2d18e084a3da41eb
Razvan Sdira <sdira_razvan@yahoo.com>
2016-11-08 20:43:08 +0200

    Add files via upload

---
+++ Exercise10.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_Data_Types
+{
+    class Exercise10
+    {
+        static void Main (string[] args)
+        {
+            int x, y;
+            bool both;
+
+            Console.Write("Input first number:");
+            x = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Input second number;");
+            y = Convert.ToInt32(Console.ReadLine());
+
+            both = ((x % 2 == 0) && (y % 2 == 0)) ? true : false;
+
+            Console.Write(both ? "Even" : "Odd");
+            Console.ReadLine();
+        }
+    }
+}
