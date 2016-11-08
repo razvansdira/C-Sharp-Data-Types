a7e38c3f96afd84f40eea58bd2a900ff975a93a4
Razvan Sdira <sdira_razvan@yahoo.com>
2016-11-08 12:22:25 +0200

    Add files via upload

---
+++ Exercise5.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_Data_Types
+{
+    class Exercise5
+    {
+        static void Main (string[] args)
+        {
+            double r, perim, area;
+            double pi = 3.14;
+
+            Console.Write("Input the radius of the circle: ");
+            r = Convert.ToDouble(Console.ReadLine());
+
+            perim = 2 * pi * r;
+            area = pi * r * r;
+
+            Console.WriteLine("Perimeter: {0}", perim);
+            Console.WriteLine("Area: {0}", area);
+
+            Console.ReadLine();
+        }
+    }
+}
