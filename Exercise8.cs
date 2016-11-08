f837023efcd71b812a631979b98d17a43fa8060f
Razvan Sdira <sdira_razvan@yahoo.com>
2016-11-08 20:10:16 +0200

    Add files via upload

---
+++ Exercise8.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_Data_Types
+{
+    class Exercise8
+    {
+        static void Main(String[] args)
+        {
+            float radius;
+            float pi =3.1415f;
+
+            Console.Write("Radius: ");
+            radius = Convert.ToSingle(Console.ReadLine());
+
+            Console.WriteLine(" Output:");
+
+            Console.WriteLine("Surface: {0}", 4*pi*(radius*radius));
+            Console.WriteLine(" Volume: {0} ", 4/3*pi*(radius*radius*radius));
+
+            Console.ReadLine();
+        }
+    }
+}
