11d6a9e63774450cdfbc43b74f2764398cc4daa2
Razvan Sdira <sdira_razvan@yahoo.com>
2016-11-08 11:28:37 +0200

    Add files via upload

---
+++ Exercise2.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_Data_Types
+{
+    class Exercise2
+    {
+        static void Main(string[] args)
+        {
+            int nr, width, height;
+
+            Console.Write("Enter a number: ");
+            nr = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Enter the desired width: ");
+            width = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Expected output:");
+
+            height = width;
+
+            for(int row=0; row<height; row++)
+            {
+                for (int column=0; column<width; column++)
+                {
+                    Console.Write(nr);
+                }
+                Console.WriteLine();
+                width--;
+            }
+            Console.ReadKey();
+        }
+    }
+}
