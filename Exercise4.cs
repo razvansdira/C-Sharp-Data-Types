78e660a3f664b1ac0f03c989540453b1c49b15b2
Razvan Sdira <sdira_razvan@yahoo.com>
2016-11-08 12:09:18 +0200

    Add files via upload

---
+++ Exercise4.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_Data_Types
+{
+    class Exercise4
+    {
+        static void Main(string[] args)
+        {
+            int a, b;
+            char op;
+
+            Console.Write("Input first number: ");
+            a = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Input second number: ");
+            b = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Input operation: ");
+            op = Convert.ToChar(Console.ReadLine());
+
+            Console.WriteLine("Operation: ");
+
+            if (op == '+')
+                Console.WriteLine("{0} + {1} = {2}", a, b, a + b);
+            else if (op == '-')
+                Console.WriteLine("{0} - {1} = {2}", a, b, a - b);
+            else if ((op == 'x') || (op == '*'))
+                Console.WriteLine("{0} * {1} = {2}", a, b, a * b);
+            else if (op == '/')
+                Console.WriteLine("{0} / {1} = {2}", a, b, a / b);
+            else
+                Console.WriteLine("Operatie gresita.");
+
+            Console.ReadLine();
+        }
+    }
+}
